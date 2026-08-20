eb4c2dd66c89cfdf728ea3dec9a83530157e8876
sweet.dh <lucky00@daou.co.kr>
2026-08-20 11:09:29 +0900

    Initial commit

---
+++ backend/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.15"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.sweet"
+version = "0.0.1-SNAPSHOT"
+description = "auth-study"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    runtimeOnly("org.postgresql:postgresql")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
